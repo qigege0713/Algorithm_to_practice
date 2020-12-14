08ceb01cd257bde2e2d0f45d25e11f7310243984
zeq <zeq886@outlook.com>
2020-12-14 14:31:12 +0800

    更新动态规划算法

--- 5-最长回文子串/Program.cs
+++ 5-最长回文子串/Program.cs
@@ -11,14 +11,55 @@ namespace _5_最长回文子串
     {
         static void Main(string[] args)
         {
+            //old commit
+            // var s = new oldSolution();
+            // string str = "abc";
+            // Console.WriteLine(s.LongestPalindrome(str));
+
             var s = new Solution();
-            string str = "abc";
+            string str = "abbdbbbba";
             Console.WriteLine(s.LongestPalindrome(str));
+
         }
     }
-
-    //算法超时
     public class Solution
+    {
+        public string LongestPalindrome(string s)
+        {
+            int n = s.Length;
+            int maxlen = 1, start = 0;
+            if (s.Length < 2) return s;
+
+            bool[,] P = new bool[n, n];
+
+            for (int j = 1; j < n; j++)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    if (s[i] != s[j])
+                    {
+                        P[i, j] = false;
+                    }
+                    else
+                    {
+                        if (j - i < 3) P[i, j] = true;
+                        else P[i, j] = P[i + 1, j - 1];
+                    }
+
+                    if(P[i,j]&&j-i+1>maxlen)
+                    {
+                        maxlen = j-i+1;
+                        start = i;
+                    }
+                }
+            }
+
+            return s.Substring(start, maxlen);
+        }
+
+    }
+    //算法超时
+    public class oldSolution
     {
         public string LongestPalindrome(string s)
         {
@@ -26,24 +67,24 @@ namespace _5_最长回文子串
             int len = 1;
             for (int i = 0; i < s.Length; i++)
             {
-                for (int j = s.Length-i; j > 1; j--)
+                for (int j = s.Length - i; j > 1; j--)
                 {
-                    if (Judg(s.Substring(i,j))&&len<j)
+                    if (Judg(s.Substring(i, j)) && len < j)
                     {
                         len = j;
                         start = i;
                     }
                 }
             }
-            return s.Substring(start,len);
+            return s.Substring(start, len);
         }
         public bool Judg(string s)
         {
             int left = 0, right = s.Length - 1;
-            while (left<right)
+            while (left < right)
             {
-                if(s[left] != s[right])
-                return false;
+                if (s[left] != s[right])
+                    return false;
                 left++;
                 right--;
             }
